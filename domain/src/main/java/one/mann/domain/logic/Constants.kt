7cbd6c9b1a3c144c9230ac15d15c042f23f0ea15
psmann <psmannx@gmail.com>
2019-08-09 16:04:11 -0400

    Moved constants into separate file

---
+++ domain/src/main/java/one/mann/domain/logic/Constants.kt
@@ -0,0 +1,17 @@
+package one.mann.domain.logic
+
+const val FLAG_OFFSET = 0x1F1E6 // Regional Indicator Symbol for letter A
+const val ASCII_OFFSET = 0x41 // Uppercase letter A
+const val DURATION_PATTERN = "H 'Hours and' m 'Minutes'"
+const val DATE_PATTERN = "d MMM, h:mm aa"
+const val DAY_PATTERN = "E"
+const val HOUR_PATTERN = "h aa"
+const val TIME_PATTERN = "h:mm aa"
+const val CELSIUS = "C"
+const val FAHRENHEIT = "F"
+const val HECTOPASCAL = " hPa"
+const val PERCENT = " %"
+const val METERS = " m"
+const val METERS_PER_SECOND = " m/s"
+const val DEGREES = "°"
+const val IMPERIAL = "imperial"
