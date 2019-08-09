10c9a4c9909e06ddc2f678d0227005721446ba35
psmann <psmannx@gmail.com>
2019-08-09 19:10:09 -0400

    Added custom qualifiers

---
+++ app/src/main/java/one/mann/weatherman/di/annotations/qualifier/OpenWeatherMap.kt
@@ -0,0 +1,7 @@
+package one.mann.weatherman.di.annotations.qualifier
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+internal annotation class OpenWeatherMap
---
+++ app/src/main/java/one/mann/weatherman/di/annotations/qualifier/Units.kt
@@ -0,0 +1,8 @@
+package one.mann.weatherman.di.annotations.qualifier
+
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+internal annotation class Units
