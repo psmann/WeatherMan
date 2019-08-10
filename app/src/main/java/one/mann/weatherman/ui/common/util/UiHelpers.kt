f7a99ee0be2f54fd113a0e49dc492513e1269161
psmann <psmannx@gmail.com>
2019-08-09 23:30:59 -0400

    Bug fix (typo)

--- app/src/main/java/one/mann/weatherman/ui/common/util/UiHelpers.kt
+++ app/src/main/java/one/mann/weatherman/ui/common/util/UiHelpers.kt
@@ -9,7 +9,7 @@ internal fun getGradient(sunPosition: Float = 0.1f, isOvercast: Boolean = false)
     in -0.050..0.075, in 0.925..1.050 -> // Dawn-Sunrise and Sunset-Twilight
         if (isOvercast) R.drawable.background_gradient_sunrise_clouds
         else R.drawable.background_gradient_sunrise_clear
-    in 0.076..0.926 -> // Day
+    in 0.076..0.924 -> // Day
         if (isOvercast) R.drawable.background_gradient_day_clouds
         else R.drawable.background_gradient_day_clear
     else -> // Night
