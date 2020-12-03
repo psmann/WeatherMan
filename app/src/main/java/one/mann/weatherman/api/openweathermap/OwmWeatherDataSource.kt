993fc63e268ceba70df52e4c9f5a9e89ff0ac4e9
Mann <psmannx@gmail.com>
2020-12-03 03:35:08 -0500

    Reformatted code

--- app/src/main/java/one/mann/weatherman/api/openweathermap/OwmWeatherDataSource.kt
+++ app/src/main/java/one/mann/weatherman/api/openweathermap/OwmWeatherDataSource.kt
@@ -12,39 +12,51 @@ import javax.inject.Inject
 
 internal class OwmWeatherDataSource @Inject constructor(private val owmWeatherService: OwmWeatherService) : WeatherDataSource {
 
-    override suspend fun getCurrentWeather(location: Location): CurrentWeather =
-            owmWeatherService.getCurrentWeather(location.coordinates[0], location.coordinates[1])
-                    .mapToDomain()
+    override suspend fun getCurrentWeather(location: Location): CurrentWeather {
+        return owmWeatherService.getCurrentWeather(location.coordinates[0], location.coordinates[1])
+                .mapToDomain()
+    }
 
-    override suspend fun getDailyForecast(location: Location): List<DailyForecast> =
-            owmWeatherService.getDailyForecast(location.coordinates[0], location.coordinates[1])
-                    .list?.map { it.mapToDomain() }!!
+    override suspend fun getDailyForecast(location: Location): List<DailyForecast> {
+        return owmWeatherService.getDailyForecast(location.coordinates[0], location.coordinates[1]).list
+                ?.map { it.mapToDomain() }!!
+    }
 
-    override suspend fun getHourlyForecast(location: Location): List<HourlyForecast> =
-            owmWeatherService.getHourlyForecast(location.coordinates[0], location.coordinates[1])
-                    .list?.map { it.mapToDomain() }!!
+    override suspend fun getHourlyForecast(location: Location): List<HourlyForecast> {
+        return owmWeatherService.getHourlyForecast(location.coordinates[0], location.coordinates[1]).list
+                ?.map { it.mapToDomain() }!!
+    }
 
     override suspend fun getAllCurrentWeather(locations: List<Location>): List<CurrentWeather> {
         val currentWeathers: MutableList<CurrentWeather> = mutableListOf()
-        for (location in locations) currentWeathers.add(
-                owmWeatherService.getCurrentWeather(location.coordinates[0], location.coordinates[1])
-                        .mapToDomain())
+        for (location in locations) {
+            currentWeathers.add(
+                    owmWeatherService.getCurrentWeather(location.coordinates[0], location.coordinates[1])
+                            .mapToDomain()
+            )
+        }
         return currentWeathers
     }
 
     override suspend fun getAllDailyForecast(locations: List<Location>): List<List<DailyForecast>> {
         val dailyForecasts: MutableList<List<DailyForecast>> = mutableListOf()
-        for (location in locations) dailyForecasts.add(
-                owmWeatherService.getDailyForecast(location.coordinates[0], location.coordinates[1])
-                        .list?.map { it.mapToDomain() }!!)
+        for (location in locations) {
+            dailyForecasts.add(
+                    owmWeatherService.getDailyForecast(location.coordinates[0], location.coordinates[1]).list
+                            ?.map { it.mapToDomain() }!!
+            )
+        }
         return dailyForecasts
     }
 
     override suspend fun getAllHourlyForecast(locations: List<Location>): List<List<HourlyForecast>> {
         val hourlyForecasts: MutableList<List<HourlyForecast>> = mutableListOf()
-        for (location in locations) hourlyForecasts.add(
-                owmWeatherService.getHourlyForecast(location.coordinates[0], location.coordinates[1])
-                        .list?.map { it.mapToDomain() }!!)
+        for (location in locations) {
+            hourlyForecasts.add(
+                    owmWeatherService.getHourlyForecast(location.coordinates[0], location.coordinates[1]).list
+                            ?.map { it.mapToDomain() }!!
+            )
+        }
         return hourlyForecasts
     }
 }
