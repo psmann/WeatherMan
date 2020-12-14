6f311155e5ef0e4ce4524897246ee1cb1c90372f
Mann <psmannx@gmail.com>
2020-12-14 02:36:07 -0500

    Updated database functions

--- interactors/src/main/java/one/mann/interactors/data/sources/framework/DatabaseDataSource.kt
+++ interactors/src/main/java/one/mann/interactors/data/sources/framework/DatabaseDataSource.kt
@@ -1,7 +1,7 @@
 package one.mann.interactors.data.sources.framework
 
 import one.mann.domain.models.NotificationData
-import one.mann.domain.models.location.Location
+import one.mann.domain.models.weather.City
 import one.mann.domain.models.weather.Weather
 
 /* Created by Psmann. */
@@ -10,11 +10,11 @@ interface DatabaseDataSource {
 
     suspend fun insertWeather(weather: Weather)
 
+    suspend fun getAllCities(): List<City>
+
     suspend fun getNotificationData(): NotificationData
 
     suspend fun getAllWeather(): List<Weather>
-
-    suspend fun getAllLocations(): List<Location>
 
     suspend fun updateAllWeather(weathers: List<Weather>)
 
