0cab440e09aab1ff720d40064ee7485481403b63
Mann <psmannx@gmail.com>
2020-12-16 02:35:07 -0500

    Updated todo list

--- app/src/main/java/one/mann/weatherman/Todo.kt
+++ app/src/main/java/one/mann/weatherman/Todo.kt
@@ -3,12 +3,6 @@ package one.mann.weatherman
 /*
 TODO:
  Fix navBar and statusBar heights
- Update Database model: Atomise and make it adhere to single responsibility principle (split-up into multiple tables)
- Make Weather data model less repetitive by getting rid of redundant operations (replace with arrays/lists)
- Use relevant data types in Weather model and database instead of using strings everywhere
- Bugs:
-  1) The same city can be added multiple times
-  2) Deleting one city also removes any other city having the same name
  Handle navBar hidden usecase (screen layouts should resize accordingly to fill the empty space)
  Add tests for all modules
  Implement CI/CD (Jenkins or CircleCI)
@@ -24,4 +18,10 @@ DONE:
  Migrate ViewPager to ViewPager2
  Get weather for current location (instead of previous saved location) in notifications if GPS is enabled on device
  Write and test Proguard rules
+ Update Database model: Atomise and make it adhere to single responsibility principle (split-up into multiple tables)
+ Make Weather data model less repetitive by getting rid of redundant operations (replace with arrays/lists)
+ Use relevant data types in Weather model and database instead of using strings everywhere
+ Bugs:
+  1) The same city can be added multiple times
+  2) Deleting one city also removes any other city having the same name
 */
