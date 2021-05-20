a0e77749443f9139f4f8660316d9ef847c1b642d
Mann <psmannx@gmail.com>
2021-05-20 02:34:01 -0400

    Fixed crash on first app start bug

--- app/src/main/java/one/mann/weatherman/framework/data/preferences/SettingsPreferencesDataSource.kt
+++ app/src/main/java/one/mann/weatherman/framework/data/preferences/SettingsPreferencesDataSource.kt
@@ -1,10 +1,10 @@
 package one.mann.weatherman.framework.data.preferences
 
 import android.content.SharedPreferences
-import androidx.core.content.edit
 import one.mann.interactors.data.sources.framework.PreferencesDataSource
-import one.mann.weatherman.ui.common.util.LAST_UPDATED_KEY
-import one.mann.weatherman.ui.common.util.SETTINGS_UNITS_KEY
+import one.mann.weatherman.common.LAST_UPDATED_KEY
+import one.mann.weatherman.common.SETTINGS_UNITS_KEY
+import one.mann.weatherman.common.SETTINGS_UNITS_DEFAULT
 import javax.inject.Inject
 
 /* Created by Psmann. */
@@ -12,19 +12,7 @@ import javax.inject.Inject
 internal class SettingsPreferencesDataSource @Inject constructor(private val preferences: SharedPreferences) :
     PreferencesDataSource {
 
-    companion object {
-        private const val UNITS = "metric"
-    }
-
-    override suspend fun getUnits(): String {
-        if (preferences.getString(SETTINGS_UNITS_KEY, "")!! == "") preferences.edit {
-            putString(
-                SETTINGS_UNITS_KEY,
-                UNITS
-            )
-        }
-        return preferences.getString(SETTINGS_UNITS_KEY, UNITS)!!
-    }
+    override suspend fun getUnits(): String = preferences.getString(SETTINGS_UNITS_KEY, SETTINGS_UNITS_DEFAULT)!!
 
     override suspend fun getLastUpdated(): Long = preferences.getLong(LAST_UPDATED_KEY, 0L)
 }
